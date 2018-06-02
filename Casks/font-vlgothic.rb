67209adfe8b6016ebc81bd15880c48a8ddc682be
HARU Akebono <haru.akebono.11@gmail.com>
2018-06-02 13:41:26 +0900

    add VL Gothic

---
+++ Casks/font-vlgothic.rb
@@ -0,0 +1,11 @@
+cask 'font-vlgothic' do
+  version '20141206'
+  sha256 :no_check
+
+  url 'https://ja.osdn.net/frs/redir.php?m=iij&f=vlgothic%2F62375%2FVLGothic-20141206.zip'
+  name 'vlgothic'
+  homepage 'http://vlgothic.dicey.org/'
+
+  font 'VLGothic/VL-Gothic-Regular.ttf'
+  font 'VLGothic/VL-PGothic-Regular.ttf'
+end
